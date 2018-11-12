3f453ea7e121f428f9f84755938fcc6564a72e54
Sergey Bakaev <bakaev@yandex.ru>
2018-11-12 22:19:12 +0300

    rollbar package was installed

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 import Koa from 'koa';
+import Rollbar from 'rollbar';
+
+const rollbar = new Rollbar('POST_SERVER_ITEM_ACCESS_TOKEN');
 
 const app = new Koa();
+
+app.use(async (ctx, next) => {
+  try {
+    await next();
+  } catch (err) {
+    rollbar.error(err, ctx.request);
+  }
+});
 
 app.use(async (ctx) => {
   ctx.body = 'Hello World';

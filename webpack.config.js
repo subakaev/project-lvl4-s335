c001bf822974c85feca6057153fef5f30d636483
Sergey Bakaev <bakaev@yandex.ru>
2018-11-12 23:21:15 +0300

    webpack base config

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+import autoprefixer from 'autoprefixer';
+
+module.exports = {
+  mode: process.env.NODE_ENV || 'development',
+  entry: ['./src/index.js'],
+  output: {
+    publicPath: '/assets/',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: 'style-loader',
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: true,
+            },
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              plugins: [
+                autoprefixer({
+                  browsers: ['ie >= 8', 'last 4 version'],
+                }),
+              ],
+              sourceMap: true,
+            },
+          },
+        ],
+      },
+    ],
+  },
+};

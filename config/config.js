1b0823c3f73930b3c71a5e2f4097b1b05b61ede9
Sergey Bakaev <bakaev@yandex.ru>
2018-11-13 22:32:35 +0300

    change prod db config

--- config/config.js
+++ config/config.js
@@ -11,10 +11,11 @@ module.exports = {
     dialect: 'mysql',
   },
   production: {
-    username: process.env.DATABASE_USERNAME,
+    username: process.env.DATABASE_USER,
     password: process.env.DATABASE_PASSWORD,
     database: process.env.DATABASE_NAME,
-    host: process.env.DATABASE_HOSTNAME,
+    host: process.env.DATABASE_HOST,
+    port: process.env.DATABASE_PORT,
     dialect: 'postgres',
   },
 };

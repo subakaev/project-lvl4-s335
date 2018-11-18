34bde88a920acf1d60d6ed671736f12f0e123d22
Sergey <bakaev@yandex.ru>
2018-11-18 21:01:11 +0300

    base routes added

--- routes/index.js
+++ routes/index.js
@@ -2,4 +2,19 @@ export default (router) => {
   router.get('root', '/', (ctx) => {
     ctx.render('index');
   });
+
+  router.get('login', '/login', (ctx) => {
+    ctx.render('auth/login');
+  });
+  router.get('register', '/register', (ctx) => {
+    ctx.render('auth/register');
+  });
+
+  router.get('users', '/users', (ctx) => {
+    ctx.render('users/index');
+  });
+
+  router.get('tasks', '/tasks', (ctx) => {
+    ctx.render('tasks/index');
+  });
 };

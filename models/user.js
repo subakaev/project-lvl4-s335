6915f8f296665855afee69776a801eba860d780a
Sergey <bakaev@yandex.ru>
2018-11-19 21:47:05 +0300

    add validation to user model

--- models/user.js
+++ models/user.js
@@ -4,16 +4,28 @@ export default (sequelize, DataTypes) => {
       type: DataTypes.STRING,
       allowNull: false,
       unique: true,
+      validate: {
+        isEmail: true,
+      },
     },
     password: {
       type: DataTypes.STRING,
       allowNull: false,
+      validate: {
+        notEmpty: true,
+      },
     },
     firstName: {
       type: DataTypes.STRING,
+      validate: {
+        notEmpty: true,
+      },
     },
     lastName: {
       type: DataTypes.STRING,
+      validate: {
+        notEmpty: true,
+      },
     },
   }, {});
 

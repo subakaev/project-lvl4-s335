5a210a1f0b7271ed07f6279b8863f15781ecf8f6
Sergey <bakaev@yandex.ru>
2018-12-01 14:41:13 +0300

    blanks routes for tags CRUD

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,8 @@
 import home from './home';
 import user from './user';
 import task from './task';
+import tags from './tags';
 
-const routes = [home, user, task];
+const routes = [home, user, task, tags];
 
 export default router => routes.forEach(x => x(router));
---
+++ routes/tags.js
@@ -0,0 +1,21 @@
+export default (router) => {
+  router.get('tags', '/tags', (ctx) => {
+    ctx.body = 'not implemented';
+  });
+
+  router.get('newTag', '/tags/new', (ctx) => {
+    ctx.body = 'not implemented';
+  });
+
+  router.post('tags', '/tags', (ctx) => {
+    ctx.body = 'not implemented';
+  });
+
+  router.put('tags', '/tags', (ctx) => {
+    ctx.body = 'not implemented';
+  });
+
+  router.delete('tags', '/tags', (ctx) => {
+    ctx.body = 'not implemented';
+  });
+};
